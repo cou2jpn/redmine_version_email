21bc8c5a99d2590c94234155e34644e7d36c2df1
cou2jpn <anemone.reef@gmil.com>
2012-01-06 11:33:50 +0900

    edit version info.

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_version_email do
   name 'Redmine Version Email plugin'
   author 'cou2jpn'
   description 'This is a plugin for Redmine that sends version email on update.'
-  version '1.0.0'
+  version '1.3.0.0'
 end
 
 Dispatcher.to_prepare do
