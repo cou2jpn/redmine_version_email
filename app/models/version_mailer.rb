65b02af1e9c607179c5bc692cabf325dfeb29685
cou2jpn <anemone.reef@gmail.com>
2015-04-22 18:14:36 +0900

    Support Redmine3.0.
    User set to mail_notification 'none' was remove from the recipients.

--- app/models/version_mailer.rb
+++ app/models/version_mailer.rb
@@ -1,39 +1,31 @@
 class VersionMailer < Mailer
 
   def version_added(version)
-    project = version.project
-    wiki_url = url_for(:controller => 'wiki', :action => 'show', :project_id => project, :id => version.wiki_page_title) unless version.wiki_page_title.blank?
-    redmine_headers 'Project' => project.identifier,
+    @version = version
+    @project = @version.project
+    @version_url = url_for(:controller => 'versions', :action => 'show', :id => @version)
+    @wiki_url = url_for(:controller => 'wiki', :action => 'show', :project_id => @project, :id => @version.wiki_page_title) unless @version.wiki_page_title.blank?
+    redmine_headers 'Project' => @project.identifier,
                     'author' => User.current.login,
-                    'version-added' => version.id
-    set_language_if_valid User.current.language
-    # send to members permitted :view_issues.
-    recipients project.members.collect {|m| m.user}.select {|user| user.allowed_to?(:view_issues, project)}.collect {|u| u.mail}
-    subject "[#{project.name}: #{l(:label_version)}] #{version.name}"
-    body :project => project,
-         :version => version,
-         :version_url => url_for(:controller => 'versions', :action => 'show', :id => version),
-         :wiki_url => wiki_url
-    render_multipart('version_added', body)
+                    'version-added' => @version.id
+    to = @project.users.select {|u| u.mail_notification != 'none' && u.allowed_to?(:view_issues, @project)}
+
+    mail :to => to, :subject => "[#{@project.name}: #{l(:label_version)} #{l(:label_added)}] #{@version.name}"
   end
 
   def version_updated(version)
-    project = version.project
-    wiki_url = url_for(:controller => 'wiki', :action => 'show', :project_id => project, :id => version.wiki_page_title) unless version.wiki_page_title.blank?
-    redmine_headers 'Project' => project.identifier,
+    @version = version
+    @project = @version.project
+    @version_url = url_for(:controller => 'versions', :action => 'show', :id => @version)
+    @effective_date_old = @version.effective_date_was.blank? ? l(:label_none) : format_date(@version.effective_date_was)
+    @effective_date_new = @version.effective_date.blank? ? l(:label_none) : format_date(@version.effective_date)
+    @wiki_url = url_for(:controller => 'wiki', :action => 'show', :project_id => @project, :id => @version.wiki_page_title) unless @version.wiki_page_title.blank?
+    redmine_headers 'Project' => @project.identifier,
                     'author' => User.current.login,
-                    'version-modified' => version.id
-    set_language_if_valid User.current.language
-    # send to members permitted :view_issues.
-    recipients project.members.collect {|m| m.user}.select {|user| user.allowed_to?(:view_issues, project)}.collect {|u| u.mail}
-    subject "[#{project.name}: #{l(:label_version)}] #{version.name}"
-    body :project => project,
-         :version => version,
-         :effective_date_old => version.effective_date_was.blank? ? l(:label_none) : format_date(version.effective_date_was),
-         :effective_date_new => version.effective_date.blank? ? l(:label_none) : format_date(version.effective_date),
-         :version_url => url_for(:controller => 'versions', :action => 'show', :id => version),
-         :wiki_url => wiki_url
-    render_multipart('version_updated', body)
+                    'version-modified' => @version.id
+    to = @project.users.select {|u| u.mail_notification != 'none' && u.allowed_to?(:view_issues, @project)}
+
+    mail :to => to, :subject => "[#{@project.name}: #{l(:label_version)} #{l(:label_modified)}] #{@version.name}"
   end
 
 end
--- init.rb
+++ init.rb
@@ -1,17 +1,14 @@
-require 'dispatcher'
-require 'redmine'
-require 'version_patch'
-require 'version_notifiable_patch'
-
 Redmine::Plugin.register :redmine_version_email do
   name 'Redmine Version Email plugin'
   author 'cou2jpn'
   description 'This is a plugin for Redmine that sends version email on update.'
-  version '1.3.2'
-  requires_redmine :version_or_higher => '1.3.0'
+  version '3.0.0'
+  requires_redmine :version_or_higher => '3.0.0'
 end
 
-Dispatcher.to_prepare do
+ActionDispatch::Callbacks.to_prepare do
+  require 'version_patch'
+  require 'version_notifiable_patch'
   Version.send(:include, VersionPatch)
 end
 
